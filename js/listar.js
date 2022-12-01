65665ebde72d237a6212c35cf884e23afd59b0c8
Joseph <joseph.carlosx@gmail.com>
2022-11-30 20:46:33 -0400

    Att

--- js/listar.js
+++ js/listar.js
@@ -8,12 +8,28 @@ fetch('https://api-aluno.meuapp.net.br/api/eventos', {
         dados.forEach(function (item) {
             document.querySelector('#dados').innerHTML += `
             <tr>
-            <td><button class="btn btn-danger">Apagar</button></td>
+            <td><button class="btn btn-danger apagar" data-id="${item.id}">Apagar</button></td>
             <td>${item.nome}</td>
             <td>${item.contato}</td>
             <td>${item.data} - ${item.hora}</td>
-            <td>${item.endereco.logradouro}, ${item.endereco.bairro}</td>
+            <td>${item.endereco?.logradouro}, ${item.endereco?.numero}</td>
             </tr>
             `;
+        });
+        adiconarEventoApagar();
+    })
+
+function adiconarEventoApagar() {
+    $('.apagar').on('click', function () {
+        var id = $(this).data('id');
+        var url = `https://api-aluno.meuapp.net.br/api/eventos/${id}`;
+        console.log(url)
+        $.ajax({
+            url: url,
+            method: 'DELETE',
+            success: function (resposta) {
+                window.location.reload();
+            }
         })
     })
+}
